b3792293ffb9c1eeee7e6bd60a71cddb6f92c4da
Tobiloba Adedeji <96334363+tobySolutions@users.noreply.github.com>
2022-12-26 22:36:04 +0000

    Add Printing and Variables Lecture

---
+++ Python/4_variablesAndPrinting.py
@@ -0,0 +1,31 @@
+# Variables
+'''
+ Variables in Python:
+ 
+ - Cannot begin with a number
+ 
+ - Cannot contain any special characters other than underscore; which is used to separate words (Snake-case). It's not conventional to use camel-case in Python.
+ 
+- Cannot contain any spaces
+'''
+number = 5
+number_in_words = "ten"
+
+print(number, number_in_words)
+
+
+
+
+
+
+# Printing
+
+# The print function takes a default "end" argument of "\n" (New line character).
+
+# print("Hello", "world")  # Hello world\n
+# print("Next line?")  # Next line?\n
+
+# Add an "end" argument to override the new line character
+
+# print("Hello", "world", end=" ")  # Hello world
+# print("Next line?", end="")  # Next line?
