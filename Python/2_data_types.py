3970f84ee706ce8d600e84b81f2af95326fdd5b7
Tobiloba Adedeji <96334363+tobySolutions@users.noreply.github.com>
2022-12-26 17:27:52 +0000

    I deleted main.py and created the Python folder then added lesson2

---
+++ Python/2_data_types.py
@@ -0,0 +1,21 @@
+'''
+There are 5 fundamental data types in Python and they are:
+
+1) int: Positive and Negative "Integers".
+2) float: Positive and Negative floating-point numbers(any number with a decimal point).
+
+3) str: Sequence of characters with ', ", """, '''''' at the start and end.
+
+4) bool: Booleans in Python have only two values which is either True or False.
+
+5) None: Like null in Javascript (Nothing)
+'''
+
+
+# Examples
+
+print(type(5)) # <class 'int'>
+print(type("Agbo")) # <class 'str'>
+print(type(True)) # <class 'bool'>
+print(type(2.6)) # <class 'float'>
+print(type(None)) # <class 'NoneType'>
