973ece7c98658ccefc4d88f256d02a62acc8842f
Tobiloba Adedeji <96334363+tobySolutions@users.noreply.github.com>
2022-12-26 18:45:40 +0000

    Add comments lesson

---
+++ Python/3_comments.py
@@ -0,0 +1,23 @@
+'''
+1. Commented code doesn't get translated by the translator (be it an interpreter or a compiler).
+
+2. You should only use comments if it is adding value to your program or making it easier to read.
+
+3. The marks for strings also work as comments
+'''
+
+# Single line comment
+
+
+'''
+ Multi-line comment
+'''
+
+"""
+Another Multiline comment
+"""
+
+
+print("Hello world")  # output stuff
+
+
