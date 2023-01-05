b6851b22ea290fd8bd7e75875d5e7341968365ac
Tobiloba Adedeji <96334363+tobySolutions@users.noreply.github.com>
2023-01-05 20:28:17 +0000

    Add conditionals chapter

---
+++ Python/10_conditionals.py
@@ -0,0 +1,49 @@
+"""
+Notes:
+  - Conditionals are used when you want to run a part of a program conditionally
+  - Indentation is important for if, elif and else statements
+  - 1 tab key press equals 4 space key presses
+"""
+
+# If, elif, else statements
+
+# number = float(input("Enter a number: "))
+
+# if number < 0 and number % 2 == 0:
+#   print("Number is less than zero, but even")
+# elif number < 10 and number % 2 == 0 and number != 2:  # I basically hate 2
+#   print("Number is less than 10, even and not 2")
+# elif number < 20 or number % 2 == 0:
+#   print("Number is less than 20 or number is the dreaded 2")
+# else:
+#   print("Number is greater than 20")
+
+# print("Finished!")
+
+
+
+# Nested conditions - Watch out for Indentation
+
+# if number < 10 and number % 1.0 == 0:  # Remainder after dividing by 1.0 must be "0"
+#    print("The second condition doesn't really make a difference")
+#    another_number = float(input("Enter a second number, " + "first number is " + str(number) + ": "))
+#    if another_number > 10:
+#      print("Second number is greater than the first number")
+#    else:
+#     print("Second number", another_number, "is within the range of the condition for first number")
+
+
+# In-line if statement
+
+# number = 4
+
+# result = "Ok, you're doing great" if number < 5 else "Write another number"
+
+# print(result)
+
+
+
+
+
+
+
